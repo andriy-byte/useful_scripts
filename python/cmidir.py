160b86d8ac649f14e9e1677d8fbe4ab40bba3ed7
Andriy <60627091+andriy-byte@users.noreply.github.com>
2023-01-02 02:26:36 +0200

    reformated description

--- python/cmidir.py
+++ python/cmidir.py
@@ -30,12 +30,20 @@ def run_command_in_dir(path: Union[Path, str], command: str, trigger: list[str],
 
 
 def main():
-    program_description = """script who iterates in directories and run command, can have trigger (specific directory or file)," 
-                          " in this case command will execute if directory contains trigger \n" 
-                          "example : python cmidir.py --path \"../some path/\" --command \"some command\" --trigger \".git\" --trigger \"file.txt\" """
+    program_description = """
+    Program description:
+    script who iterates in directories and run command, can have trigger (specific directory or file), 
+    in this case command will execute if directory contains trigger \n
+    
+    example : 
+        
+        python cmidir.py --path "../some path/" --command "some command"  --trigger ".git" --trigger "file.txt" 
+        
+        python g:\scripts\python\cmidir.py --command "git pull" --path "g:\cool" --ignore-folder "linux" --ignore-folder "tabulate" --trigger ".git"
+    """
 
     parser = argparse.ArgumentParser(
-        prog="Iterate and execute with triggers", description=program_description)
+        prog="Iterate and execute with triggers",formatter_class=argparse.RawDescriptionHelpFormatter, description=program_description)
     parser.add_argument("--path", type=str, dest="path", help="input path where script will run",
                         required=True)
     parser.add_argument("--command", type=str, dest="command",
